1f495f47984525b7a5b73582b73dd82119b0cf53
testdariasav <testdariasav@gmail.com>
2025-02-11 22:31:18 -0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import { Inter } from 'next/font/google'
  
 // If loading a variable font, you don't need to specify the font weight
-const inter = Inter({ subsets: ['ital'] })
+const inter = Inter({ subsets: ['Roboto'] })
  
 export default function MyApp({ Component, pageProps }) {
   return (
