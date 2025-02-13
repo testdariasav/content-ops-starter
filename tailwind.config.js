f908914283228c252bd863ae0f95a4096d11b382
testdariasav <testdariasav@gmail.com>
2025-02-13 10:38:57 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -61,48 +61,48 @@ module.exports = {
                 // 'h1,h2,h3,h4,h5,h6,blockquote': {
                 //     fontFamily: theme(`fontFamily.${themeStyle.fontHeadlines}`)
                 // },
-                // 'h1,.h1': {
-                //     fontSize: theme(`fontSize.${themeStyle.h1.size}`),
-                //     fontWeight: theme(`fontWeight.${themeStyle.h1.weight}`),
-                //     letterSpacing: theme(`letterSpacing.${themeStyle.h1.letterSpacing}`),
-                //     textDecoration: themeStyle.h1.decoration,
-                //     textTransform: themeStyle.h1.case
-                // },
-                // 'h2,.h2': {
-                //     fontSize: theme(`fontSize.${themeStyle.h2.size}`),
-                //     fontWeight: theme(`fontWeight.${themeStyle.h2.weight}`),
-                //     letterSpacing: theme(`letterSpacing.${themeStyle.h2.letterSpacing}`),
-                //     textDecoration: themeStyle.h2.decoration,
-                //     textTransform: themeStyle.h2.case
-                // },
-                // 'h3,.h3': {
-                //     fontSize: theme(`fontSize.${themeStyle.h3.size}`),
-                //     fontWeight: theme(`fontWeight.${themeStyle.h3.weight}`),
-                //     letterSpacing: theme(`letterSpacing.${themeStyle.h3.letterSpacing}`),
-                //     textDecoration: themeStyle.h3.decoration,
-                //     textTransform: themeStyle.h3.case
-                // },
-                // 'h4,.h4': {
-                //     fontSize: theme(`fontSize.${themeStyle.h4.size}`),
-                //     fontWeight: theme(`fontWeight.${themeStyle.h4.weight}`),
-                //     letterSpacing: theme(`letterSpacing.${themeStyle.h4.letterSpacing}`),
-                //     textDecoration: themeStyle.h4.decoration,
-                //     textTransform: themeStyle.h4.case
-                // },
-                // h5: {
-                //     fontSize: theme(`fontSize.${themeStyle.h5.size}`),
-                //     fontWeight: theme(`fontWeight.${themeStyle.h5.weight}`),
-                //     letterSpacing: theme(`letterSpacing.${themeStyle.h5.letterSpacing}`),
-                //     textDecoration: themeStyle.h5.decoration,
-                //     textTransform: themeStyle.h5.case
-                // },
-                // h6: {
-                //     fontSize: theme(`fontSize.${themeStyle.h6.size}`),
-                //     fontWeight: theme(`fontWeight.${themeStyle.h6.weight}`),
-                //     letterSpacing: theme(`letterSpacing.${themeStyle.h6.letterSpacing}`),
-                //     textDecoration: themeStyle.h6.decoration,
-                //     textTransform: themeStyle.h6.case
-                // }
+                'h1,.h1': {
+                    fontSize: theme(`fontSize.${themeStyle.h1.size}`),
+                    fontWeight: theme(`fontWeight.${themeStyle.h1.weight}`),
+                    letterSpacing: theme(`letterSpacing.${themeStyle.h1.letterSpacing}`),
+                    textDecoration: themeStyle.h1.decoration,
+                    textTransform: themeStyle.h1.case
+                },
+                'h2,.h2': {
+                    fontSize: theme(`fontSize.${themeStyle.h2.size}`),
+                    fontWeight: theme(`fontWeight.${themeStyle.h2.weight}`),
+                    letterSpacing: theme(`letterSpacing.${themeStyle.h2.letterSpacing}`),
+                    textDecoration: themeStyle.h2.decoration,
+                    textTransform: themeStyle.h2.case
+                },
+                'h3,.h3': {
+                    fontSize: theme(`fontSize.${themeStyle.h3.size}`),
+                    fontWeight: theme(`fontWeight.${themeStyle.h3.weight}`),
+                    letterSpacing: theme(`letterSpacing.${themeStyle.h3.letterSpacing}`),
+                    textDecoration: themeStyle.h3.decoration,
+                    textTransform: themeStyle.h3.case
+                },
+                'h4,.h4': {
+                    fontSize: theme(`fontSize.${themeStyle.h4.size}`),
+                    fontWeight: theme(`fontWeight.${themeStyle.h4.weight}`),
+                    letterSpacing: theme(`letterSpacing.${themeStyle.h4.letterSpacing}`),
+                    textDecoration: themeStyle.h4.decoration,
+                    textTransform: themeStyle.h4.case
+                },
+                h5: {
+                    fontSize: theme(`fontSize.${themeStyle.h5.size}`),
+                    fontWeight: theme(`fontWeight.${themeStyle.h5.weight}`),
+                    letterSpacing: theme(`letterSpacing.${themeStyle.h5.letterSpacing}`),
+                    textDecoration: themeStyle.h5.decoration,
+                    textTransform: themeStyle.h5.case
+                },
+                h6: {
+                    fontSize: theme(`fontSize.${themeStyle.h6.size}`),
+                    fontWeight: theme(`fontWeight.${themeStyle.h6.weight}`),
+                    letterSpacing: theme(`letterSpacing.${themeStyle.h6.letterSpacing}`),
+                    textDecoration: themeStyle.h6.decoration,
+                    textTransform: themeStyle.h6.case
+                }
             });
             addComponents({
                 '.sb-component-button-primary': {
