e549c7cf76c189122b6a7685a6a5667b60014ca8
testdariasav <testdariasav@gmail.com>
2025-02-13 09:47:12 -0500

    Update _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,8 @@
-import { Inter } from 'next/font/google'
 import '../css/main.css';
-import { Roboto } from 'next/font/google'
-
-const roboto = Roboto({
-  weight: '400',
-  subsets: ['latin'],
-})
+import { Rochester  } from 'next/font/google'
  
 // If loading a variable font, you don't need to specify the font weight
-const inter = Inter({ subsets: ['latin'] })
+const inter = Rochester({ subsets: ['latin'] })
 console.log('loading fonts')
  
 export default function MyApp({ Component, pageProps }) {
